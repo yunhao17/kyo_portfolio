99776b62cb93665415371804de0d53d617efadfe
yunhao17 <yunhao17@gmail.com>
2021-07-07 06:21:06 +0000

    [devise機能]

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
+  
+  devise_for :admins, :controllers => {
+  :sessions => 'admins/sessions'
+  }
+  
+  devise_for :customers, :controllers => {
+  :sessions => 'customers/sessions',
+  :registrations => 'customers/registrations',
+  }
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
