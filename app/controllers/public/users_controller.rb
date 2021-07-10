3844e455c656d4500a90a61e6bca7fd51c4fc496
yunhao17 <yunhao17@gmail.com>
2021-07-10 03:53:52 +0000

    [user機能]

--- app/controllers/public/users_controller.rb
+++ app/controllers/public/users_controller.rb
@@ -6,4 +6,32 @@ class Public::UsersController < ApplicationController
   def about
   end
   
+  def index
+    @users = User.all
+  end
+  
+  def show
+    @user = User.find(params[:id])
+  end
+  
+  def edit
+    @user = User.find(params[:id])
+    if @user == current_user
+      render :edit
+    else
+      redirect_to user_path(current_user.id)
+    end
+  end
+  
+  def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+    redirect_to user_path(@user.id)
+  end
+  
+  private
+
+  def user_params
+    params.require(:user).permit(:age, :name)
+  end
 end
