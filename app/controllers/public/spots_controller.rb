77fc86999a9086fdbdd1131e7da7e167ec6e5b8d
yunhao17 <yunhao17@gmail.com>
2021-07-15 10:10:17 +0000

    [fix]

--- app/controllers/public/spots_controller.rb
+++ app/controllers/public/spots_controller.rb
@@ -3,15 +3,15 @@ class Public::SpotsController < ApplicationController
   def index
     if params[:sort] == "favo"
       # n + 1 問題が発生しているので可能であれば改善してみる
-      @spots = Spot.all.sort{|a,b| b.favorited_users.count <=> a.favorited_users.count}.page(params[:page]).per(8)
-    else 
+      @spots = Spot.all.page(params[:page]).per(8).sort{|a,b| b.favorited_users.count <=> a.favorited_users.count}
+    else
       @spots = Spot.all.page(params[:page]).per(8)
     end
   end
 
   def eria_mokuteki
     if params[:sort] == "favo"
-      @spots = Spot.where(area_id: params[:area_id] ,purpose_id: params[:purpose_id]).sort{|a,b| b.favorited_users.count <=> a.favorited_users.count}.page(params[:page]).per(8)
+      @spots = Spot.where(area_id: params[:area_id] ,purpose_id: params[:purpose_id]).page(params[:page]).per(8).sort{|a,b| b.favorited_users.count <=> a.favorited_users.count}
     else
       @spots = Spot.where(area_id: params[:area_id] ,purpose_id: params[:purpose_id]).page(params[:page]).per(8)
     end
@@ -20,7 +20,7 @@ class Public::SpotsController < ApplicationController
 
   def mokuteki
     if params[:sort] == "favo"
-      @spots = Spot.where(purpose_id: params[:purpose_id]).sort{|a,b| b.favorited_users.count <=> a.favorited_users.count}.page(params[:page]).per(8)
+      @spots = Spot.where(purpose_id: params[:purpose_id]).page(params[:page]).per(8).sort{|a,b| b.favorited_users.count <=> a.favorited_users.count}
     else
       @spots = Spot.where(purpose_id: params[:purpose_id]).page(params[:page]).per(8)
     end
