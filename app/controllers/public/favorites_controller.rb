1b506bf4899e132067d140cb03fed5485ee42a24
yunhao17 <yunhao17@gmail.com>
2021-07-18 09:38:40 +0000

    [いいね機能の非同期通信]

--- app/controllers/public/favorites_controller.rb
+++ app/controllers/public/favorites_controller.rb
@@ -4,14 +4,12 @@ class Public::FavoritesController < ApplicationController
     @spot = Spot.find(params[:spot_id])
     @favorite = current_user.favorites.new(spot_id: @spot.id)
     @favorite.save
-    redirect_to spot_path(params[:spot_id])
   end
 
   def destroy
     @spot = Spot.find(params[:spot_id])
     @favorite = current_user.favorites.find_by(spot_id: @spot.id)
     @favorite.destroy
-    redirect_to spot_path(params[:spot_id])
   end
     
 end
