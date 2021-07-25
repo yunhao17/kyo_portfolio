c893e34864d6f0881a52913717bf3b599cc48532
yunhao17 <yunhao17@gmail.com>
2021-07-25 12:36:21 +0000

    [fix]

--- app/controllers/public/searches_controller.rb
+++ app/controllers/public/searches_controller.rb
@@ -3,7 +3,7 @@ class Public::SearchesController < ApplicationController
   before_action :authenticate_user!
 
   def search
-    @spots = Spot.search(params[:keyword])
+    @spots = Spot.search(params[:keyword]).page(params[:page]).per(5)
     @keyword = params[:keyword]
     render 'searchs'
   end
