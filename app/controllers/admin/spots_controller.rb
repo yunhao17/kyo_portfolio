43681b4c920a061d9cd2f85ac5523a7283e33647
yunhao17 <yunhao17@gmail.com>
2021-07-25 09:13:32 +0000

    [管理者側レイアウト修正]

--- app/controllers/admin/spots_controller.rb
+++ app/controllers/admin/spots_controller.rb
@@ -3,7 +3,7 @@ class Admin::SpotsController < ApplicationController
   before_action :authenticate_admin!
 
   def index
-    @spots = Spot.all.page(params[:page]).per(5)
+    @spots = Spot.all.page(params[:page]).per(6)
     @areas = Area.all
     @purposes = Purpose.all
   end
